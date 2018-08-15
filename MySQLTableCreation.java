ab44f4d0bc829720823d55d6171b0b7fd2409a83
daniux <victorxdy@gmail.com>
2018-08-15 00:59:57 -0700

    Update MySQLTableCreation.java

--- MySQLTableCreation.java
+++ MySQLTableCreation.java
@@ -68,7 +68,7 @@ public class MySQLTableCreation {
 			
 			// Step 4: insert data
 			// Create a fake user
-			sql = "INSERT INTO users " + "VALUES (\"1111\", \"3229c1097c00d497a0fd282d586be050\", \"John\", \"Smith\")";
+			sql = "INSERT INTO users " + "VALUES (\"1234\", \"nopasswordhere\", \"Michael\", \"Jackson\")";
 
 			System.out.println("Executing query:\n" + sql);
 			stmt.executeUpdate(sql);
